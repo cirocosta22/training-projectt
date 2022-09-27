865876e92236feee0088ca81ca9d11722576eab6
cirocosta22 <cirinhosilva15@gmail.com>
2022-09-27 16:47:01 -0300

    discount generator

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,141 @@
-const habitante = () => {
-  const pessoas = [
-    { name: 'ciro', salario: 2300, filhos: 3 },
-    { name: 'vera', salario: 2400, filhos: 7 },
-    { name: 'Ramon', salario: 1300, filhos: 4 },
-    { name: 'Bartolomeu', salario: 1500, filhos: 2 }
-  ]
-
-  let totalSalario = 0
-
-  pessoas.forEach(pessoa => {
-    totalSalario += pessoa.salario
-  })
-  let percapta = totalSalario / pessoas.length
-  if (
-    pessoas[1].salario > pessoas[0].salario &&
-    pessoas[1].salario > pessoas[3].salario &&
-    pessoas[1].salario > pessoas[2].salario
-  ) {
-    console.log(`maior salario mensal é o de ${pessoas[1].name}`)
-  } else {
-    console.log('pessoas')
+const conta = {
+  verificaSeTemDesconto(comand) {
+    return (
+      comand.cliente === 'Ciro' &&
+      comand.primeiracompra === true &&
+      comand.pagamento === true &&
+      comand.valorTotal > 1000
+    )
+  },
+  verificaSeValorMenorQueMil(comand) {
+    return (
+      comand.cliente === 'vera' &&
+      comand.primeiracompra === true &&
+      comand.pagamento === true &&
+      comand.valorTotal < 1000 &&
+      comand.valorTotal > 500
+    )
+  },
+  verificaSeCompraMenorQuequinhentos(comand) {
+    return (
+      comand.cliente === 'Ramon' &&
+      comand.primeiracompra === true &&
+      comand.pagamento === true &&
+      comand.valorTotal < 500
+    )
+  },
+  verificaParcelamento(comand) {
+    return (
+      comand.cliente === 'Lucas' &&
+      comand.primeiracompra === true &&
+      comand.pagamento === false &&
+      comand.valorTotal > 1000
+    )
+  },
+  casoCinco(comand) {
+    return (
+      comand.cliente === 'Juliana' &&
+      comand.primeiracompra === true &&
+      comand.pagamento === false &&
+      comand.valorTotal < 1000 &&
+      comand.valor > 500
+    )
+  },
+  casoSeis(comand) {
+    return (
+      comand.cliente === 'Deborah' &&
+      comand.primeiracompra === true &&
+      comand.pagamento === false &&
+      comand.valorTotal < 500
+    )
+  },
+  casoSete(comand) {
+    return (
+      comand.cliente === 'Joao' &&
+      comand.primeiracompra === false &&
+      comand.pagamento === true &&
+      comand.valorTotal > 1000
+    )
+  },
+  casoOito(comand) {
+    return (
+      comand.cliente === 'Eva' &&
+      comand.primeiracompra === false &&
+      comand.pagamento === true &&
+      comand.valorTotal < 1000 &&
+      comand.valorTotal > 500
+    )
+  },
+  casoNove(comand) {
+    return (
+      comand.cliente === 'Jose' &&
+      comand.primeiracompra === false &&
+      comand.pagamento === true &&
+      comand.valorTotal < 500
+    )
+  },
+  casoDez(comand) {
+    return (
+      comand.cliente === 'Natanael' &&
+      comand.primeiracompra === false &&
+      comand.pagamento === false &&
+      comand.valorTotal > 1000
+    )
   }
 }
-habitante()
 
-// problema 5 resolvido
+const geradorDesconto = comand => {
+  if (conta.verificaSeTemDesconto(comand)) {
+    return `cliente tem direito a ${
+      (comand.valorTotal * 30) / 100
+    } R$ de desconto`
+  } else if (conta.verificaSeValorMenorQueMil(comand)) {
+    return (comand.valorTotal * 25) / 100
+  } else if (conta.verificaSeCompraMenorQuequinhentos(comand)) {
+    return `cliente tem direito a ${
+      (comand.valorTotal * 20) / 100
+    } R$ de desconto`
+  } else if (conta.verificaParcelamento(comand)) {
+    return `cliente tem direito a ${
+      (comand.valorTotal * 20) / 100
+    } R$ de desconto`
+  } else if (conta.casoCinco(comand)) {
+    return `cliente tem direito a ${
+      (comand.valorTotal * 15) / 100
+    } R$ de desconto`
+  } else if (conta.casoSeis(comand)) {
+    return `cliente tem direito a ${
+      (comand.valorTotal * 10) / 100
+    } R$ de desconto`
+  } else if (conta.casoSete(comand)) {
+    return `cliente tem direito a ${
+      (comand.valorTotal * 20) / 100
+    } R$ de desconto`
+  } else if (conta.casoOito(comand)) {
+    return `obrigado pela compra , volte sempre // valor sem desconto ${
+      comand.valorTotal
+    } e com desconto ${Math.abs(
+      (comand.valorTotal * 15) / 100 - comand.valorTotal
+    )}`
+  } else if (conta.casoNove(comand)) {
+    return `obrigado pela compra ${comand.cliente} no valor de ${
+      comand.valorTotal
+    }, com o desconto de 10 % ficou ${Math.abs(
+      (comand.valorTotal * 10) / 100 - comand.valorTotal
+    )}`
+  } else if (conta.casoDez(comand)) {
+    return `obrigado pela compra ${comand.cliente} sua compra no valor de ${
+      comand.valorTotal
+    }, com desconto de 10% ficou de ${Math.abs(
+      (comand.valorTotal * 10) / 100 - comand.valorTotal
+    )}`
+  }
+}
+console.log(
+  geradorDesconto({
+    cliente: 'Natanael',
+    primeiracompra: false,
+    pagamento: false,
+    valorTotal: 1360
+  })
+)
